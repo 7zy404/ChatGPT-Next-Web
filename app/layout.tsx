a5d99056f00595b6710f7729985dbebfd3522d96
7zy404 <134850667+7zy404@users.noreply.github.com>
2023-05-28 22:12:20 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "律帮Ai",
+  description: "www.lvbangai.com",
   viewport: {
     width: "device-width",
     initialScale: 1,
